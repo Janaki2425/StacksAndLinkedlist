041743b4d1ad1422c2440ce9c70f66f72b090649
Janu shankar <janakiharishankar24@gmail.com>
2022-01-21 23:02:30 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace StacksAndQueues;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        StackLinkedlist push = new StackLinkedlist();
+        push.Push();
+        StackLinkedlist pop = new StackLinkedlist();
+        pop.Pop();
+        StackLinkedlist peek = new StackLinkedlist();
+        peek.Peek();
+        StackLinkedlist display = new StackLinkedlist();
+        display.Display();
+        StackLinkedlist isempty = new StackLinkedlist();
+        isempty.Isempty();
+        Queue enqueue= new Queue();
+        enqueue.Enqueue();
+        Queue dequeue= new Queue();
+        dequeue.Dequeue();
+    }
+}
+
+
+
