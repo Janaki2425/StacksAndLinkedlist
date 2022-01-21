09e027717c411643c629c240ca8c63420d97f01d
Janu shankar <janakiharishankar24@gmail.com>
2022-01-21 23:52:42 +0530

    commit

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Linkedlistprg
+{
+    class Main
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist linkedlist = new Linkedlist();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+
+
+
+
+
+        }
+    }
+}
