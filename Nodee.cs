44f8ef4b67959450a546bd7dfd99f33dd83b7632
Janu shankar <janakiharishankar24@gmail.com>
2022-01-21 23:53:30 +0530

    commit

---
+++ Nodee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlistprg
+{
+    internal class Node
+    {
+        internal Node next;
+        private int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
